944c4805d15906f38e06f130e9282dbbaed11e95
MatiasAustin <tiasaustin32@gmail.com>
2025-11-29 22:42:51 +0700

    logo admin

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -2,7 +2,7 @@
     style="display: flex; justify-content: space-between; align-items: center;">
     <div class="admin-brand">
         <a href="{{ url('/') }}">
-            <img src="{{ asset('media/anm-logo.png') }}" alt="Admin Logo" width="40" height="auto">
+            <img src="{{ asset('media/MA-favicon-beige.png') }}" alt="Admin Logo" width="40" height="auto">
         </a>
     </div>
     <div class="admin-actions">
